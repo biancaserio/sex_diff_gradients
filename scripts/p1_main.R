c58031247b2894ed4e753b454d324bd0eade59da
biancaserio <bianca.serio@gmail.com>
2022-10-25 20:13:01 +0200

    sex differences hcp lmer controling for fam and fam*twinstatus as fixed effects: model = ~ G1_eigenvalues ~ sex + age + icv + (1 | family_id) + (1 | family_id:twin_status)

--- scripts/p1_main.R
+++ scripts/p1_main.R
@@ -48,7 +48,7 @@ setwd(codedir)
 
 ### Linear Regression GSP
 
-lm.gsp.sex_age_icv <- function(df_dv, df_iv) {
+lm.gsp_sex_contrast <- function(df_dv, df_iv) {
 
 '
   - fits and runs linear model to test for SEX effects, including sex, age and ICV in the model as covariates
@@ -88,10 +88,10 @@ return(output_df)
 
 
 
-lmer.sex_age_icv_ctrl_related <- function(df_dv, df_iv) {
+lmer.hcp_sex_contrast <- function(df_dv, df_iv) {
   
   '
-    - fits and runs linear model to test for SEX effects, including sex, age and ICV in the model as covariates
+    - fits and runs linear model to test for SEX effects, including sex, age and ICV, as well as random effects family id and family id * twin status, in the model as covariates
     - to supply: df_dv (dataframe containing the dependent variable), df_iv (dataframe containing the independent variables)
     - outputs dataframe containing t-values, p-values, and FDR-corrected q-values for SEX contrast
   '
@@ -106,14 +106,14 @@ lmer.sex_age_icv_ctrl_related <- function(df_dv, df_iv) {
   for (i in seq_along(df_dv)) {
     
     family_id = df_iv$Family_ID
+    twin_status = df_iv$TwinStatus
     
-    # Fit a linear mixed effects model: lm = Gradient_Eigenvalues ~ Sex + Age + ICV + controlling for family relatedness 
-    lmer_fit <- lmer(df_dv[[i]] ~ df_iv$Gender + df_iv$Age_in_Yrs + df_iv$FS_IntraCranial_Vol + (1 | family_id), REML = FALSE)  
-    #ProcSpeed_raw ~ age_months + sex_female + (1 | Pair) + (1 | M), REML = FALSE, data = ses01)
+    # Fit a linear mixed effects model: lm = Gradient_Eigenvalues ~ Sex + Age + ICV + random effect(family relatedness) + random effect (family relatedness * twin status)
+    # error message: https://stackoverflow.com/questions/60028673/lme4-error-boundary-singular-fit-see-issingular -> Your model did fit, but it generated that warning because your random effects are very small
+    # https://stats.stackexchange.com/questions/96600/interactions-between-random-effects
+    # https://stackoverflow.com/questions/71340764/interaction-between-two-factors-as-random-effects-in-mixed-model-in-r
     
-    # controling for twin pairs
-    #lmer_fit <- lmer(ProcSpeed_raw ~ age_months + sex_female + (1 | Pair) + (1 | M), REML = FALSE, data = ses01)
-    
+    lmer_fit <- lmer(df_dv[[i]] ~ df_iv$Gender + df_iv$Age_in_Yrs + df_iv$FS_IntraCranial_Vol + (1 | family_id) + (1 | family_id:twin_status), REML = FALSE)
     
     # Extract from summary of lmer_fit the t- and p-values
     # summary(lmer_fit)$coefficients[row, column]; row = 1 intercept, 2 sex, 3 Age, 4 ICV; columns = 1 Estimate, 2 Std. Error, 3 df, 4 t-value, 5 p-value
@@ -159,7 +159,7 @@ dim(HCP_array_aligned_G1)
 GSP_demographics_cleaned = read.csv(paste(resdir_gsp, 'demographics_cleaned.csv', sep = ''), fileEncoding = 'UTF-8-BOM')
 HCP_demographics_cleaned = read.csv(paste(resdir_hcp, 'demographics_cleaned.csv', sep = ''), fileEncoding = 'UTF-8-BOM')
 
-
+str(HCP_demographics_cleaned,  list.len=ncol(HCP_demographics_cleaned))
 
 ----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 # LINEAR REGRESSIONS
@@ -168,33 +168,29 @@ HCP_demographics_cleaned = read.csv(paste(resdir_hcp, 'demographics_cleaned.csv'
 ### GSP: model = Gradient_Eigenvalues ~ Sex + Age + ICV 
 
 # run model
-GSP_lm_G1_res = lm.gsp.sex_age_icv(df_dv = GSP_array_aligned_G1, df_iv = GSP_demographics_cleaned)
-GSP_lm_G2_res = lm.gsp.sex_age_icv(df_dv = GSP_array_aligned_G2, df_iv = GSP_demographics_cleaned)
-GSP_lm_G3_res = lm.gsp.sex_age_icv(df_dv = GSP_array_aligned_G3, df_iv = GSP_demographics_cleaned)
+GSP_lm_G1_sex_contrast_res = lm.gsp_sex_contrast(df_dv = GSP_array_aligned_G1, df_iv = GSP_demographics_cleaned)
+GSP_lm_G2_sex_contrast_res = lm.gsp_sex_contrast(df_dv = GSP_array_aligned_G2, df_iv = GSP_demographics_cleaned)
+GSP_lm_G3_sex_contrast_res = lm.gsp_sex_contrast(df_dv = GSP_array_aligned_G3, df_iv = GSP_demographics_cleaned)
 
 
 # number of significant parcels
-sum(GSP_lm_G1_res$q_val_sex < 0.05, na.rm=TRUE)  # other way: length(which(GSP_lm_G1_res$q_val_sex < 0.05))
-sum(GSP_lm_G2_res$q_val_sex < 0.05, na.rm=TRUE)  
-sum(GSP_lm_G3_res$q_val_sex < 0.05, na.rm=TRUE)  
+sum(GSP_lm_G1_sex_contrast_res$q_val_sex < 0.05, na.rm=TRUE)  # other way: length(which(GSP_lm_G1_res$q_val_sex < 0.05))
+sum(GSP_lm_G2_sex_contrast_res$q_val_sex < 0.05, na.rm=TRUE)  
+sum(GSP_lm_G3_sex_contrast_res$q_val_sex < 0.05, na.rm=TRUE)  
 
 
 
-lmer.sex_age_icv_ctrl_related
-
-
-### model = Gradient_Eigenvalues ~ Sex + Age + ICV + controlling for family relatedness
+### HCP: model = Gradient_Eigenvalues ~ Sex + Age + ICV + random effect family relatedness + random effect (family relatedness * twin status) 
 
 # run model
-
-lm_G1_sex_age_icv_ctrl_related_res = lmer.sex_age_icv_ctrl_related(df_dv = HCP_array_aligned_G1, df_iv = HCP_demographics_cleaned)
-lm_G2_sex_age_icv_ctrl_related_res = lmer.sex_age_icv_ctrl_related(df_dv = HCP_array_aligned_G2, df_iv = HCP_demographics_cleaned)
-lm_G3_sex_age_icv_ctrl_related_res = lmer.sex_age_icv_ctrl_related(df_dv = HCP_array_aligned_G3, df_iv = HCP_demographics_cleaned)
+HCP_lmer_G1_sex_contrast_res = lmer.hcp_sex_contrast(df_dv = HCP_array_aligned_G1, df_iv = HCP_demographics_cleaned)
+HCP_lmer_G2_sex_contrast_res = lmer.hcp_sex_contrast(df_dv = HCP_array_aligned_G2, df_iv = HCP_demographics_cleaned)
+HCP_lmer_G3_sex_contrast_res = lmer.hcp_sex_contrast(df_dv = HCP_array_aligned_G3, df_iv = HCP_demographics_cleaned)
 
 # number of significant parcels
-sum(lm_G1_sex_age_icv_ctrl_related_res$q_val_sex < 0.05, na.rm=TRUE)  # other way: length(which(G1_lm_res$q_val_sex < 0.05))
-sum(lm_G2_sex_age_icv_ctrl_related_res$q_val_sex < 0.05, na.rm=TRUE)  
-sum(lm_G3_sex_age_icv_ctrl_related_res$q_val_sex < 0.05, na.rm=TRUE)  
+sum(HCP_lmer_G1_sex_contrast_res$q_val_sex < 0.05, na.rm=TRUE)  # other way: length(which(G1_lm_res$q_val_sex < 0.05))
+sum(HCP_lmer_G2_sex_contrast_res$q_val_sex < 0.05, na.rm=TRUE)  
+sum(HCP_lmer_G3_sex_contrast_res$q_val_sex < 0.05, na.rm=TRUE)  
 
 
 
@@ -203,9 +199,19 @@ sum(lm_G3_sex_age_icv_ctrl_related_res$q_val_sex < 0.05, na.rm=TRUE)
 # EXPORT RESULTS 
 ----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
-write.csv(GSP_lm_G1_res, paste(resdir_gsp, 'R_lm_G1_res.csv', sep = ''), row.names = FALSE)
-write.csv(GSP_lm_G2_res, paste(resdir_gsp, 'R_lm_G2_res.csv', sep = ''), row.names = FALSE)
-write.csv(GSP_lm_G3_res, paste(resdir_gsp, 'R_lm_G3_res.csv', sep = ''), row.names = FALSE)
+write.csv(GSP_lm_G1_sex_contrast_res, paste(resdir_gsp, 'R_lm_G1_sex_contrast_res.csv', sep = ''), row.names = FALSE)
+write.csv(GSP_lm_G2_sex_contrast_res, paste(resdir_gsp, 'R_lm_G2_sex_contrast_res.csv', sep = ''), row.names = FALSE)
+write.csv(GSP_lm_G3_sex_contrast_res, paste(resdir_gsp, 'R_lm_G3_sex_contrast_res.csv', sep = ''), row.names = FALSE)
+
+
+write.csv(HCP_lmer_G1_sex_contrast_res, paste(resdir_hcp, 'R_lmer_G1_sex_contrast_res.csv', sep = ''), row.names = FALSE)
+write.csv(HCP_lmer_G2_sex_contrast_res, paste(resdir_hcp, 'R_lmer_G2_sex_contrast_res.csv', sep = ''), row.names = FALSE)
+write.csv(HCP_lmer_G3_sex_contrast_res, paste(resdir_hcp, 'R_lmer_G3_sex_contrast_res.csv', sep = ''), row.names = FALSE)
+
+
+
+
+
 
 
 
