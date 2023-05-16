47744dfd6ba0f19648d1c00781197c4cb2963f0f
biancaserio <bianca.serio@gmail.com>
2023-05-16 23:13:04 +0200

    analyses sex differences in strength of profiles and geodesic distance

--- scripts/p1_main.R
+++ scripts/p1_main.R
@@ -189,6 +189,52 @@ lmer.hcp_geo_contrast <- function(df_dv, df_iv, df_geo) {
 
 
 
+### Linear Regression HCP Connectivity lmer = Gradient_Eigenvalues ~ Sex + Age + ICV + MPC G1 + random nested effect(family relatedness/twin status) 
+
+# MPC G1 contrast
+lmer.hcp_mpc_contrast <- function(df_dv, df_iv, df_mpc) {
+  
+  '
+    - fits and runs linear model to test for MPC G1 effects, including sex, age, MPC G1 and ICV, as well as random nested effect(family relatedness/twin status in the model as covariates
+    - to supply: df_dv (dataframe containing the dependent variable), df_iv (dataframe containing the independent variables), df_mpc (dataframe containing MPC G1)
+    - outputs dataframe containing t-values, p-values, and FDR-corrected q-values for SEX contrast
+  '
+  
+  # Create empty vectors (0s) of type "double precision" and length of len(df_dv) 
+  t_val = vector(mode = "double", length = ncol(df_dv))  
+  p_val = vector(mode = "double", length = ncol(df_dv))
+  beta_val = vector(mode = "double", length = ncol(df_dv))
+  
+  
+  # Loop over the df_dv columns (= parcels)
+  for (i in seq_along(df_dv)) {
+    
+    family_id = df_iv$Family_ID
+    twin_status = df_iv$TwinStatus
+    
+    # Fit a linear mixed effects model: DV ~ Sex + Age + ICV + random nested effect(family relatedness/twin status)
+    
+    # Model including both "single" random effects included in the interaction random effect (Sofie's decision)
+    lmer_fit <- lmer(df_dv[[i]] ~ df_iv$Gender + df_iv$Age_in_Yrs + df_iv$FS_IntraCranial_Vol + df_mpc[[i]] + (1 | family_id/twin_status), REML = FALSE)
+    
+    # Extract from summary of lmer_fit the t- and p-values
+    # summary(lmer_fit)$coefficients[row, column]; row = 1 intercept, 2 sex, 3 age, 4 ICV, 5 MPC G1; columns = 1 Estimate, 2 Std. Error, 3 df, 4 t-value, 5 p-value
+    t_val[[i]] = summary(lmer_fit)$coefficients[5,4]
+    p_val[[i]] = summary(lmer_fit)$coefficients[5,5]
+    beta_val[[i]] = summary(lmer_fit)$coefficients[5,1]
+    
+  }
+  
+  # Calculate FDR-corrected q-values from p-values
+  q_val = p.adjust(p_val, method = "fdr")
+  
+  # Create output dataframe containing t-values, p-values, and q-values
+  output_df = data.frame(t_val, p_val, q_val, beta_val)
+  
+  return(output_df)
+}
+
+
 
 ### Linear Regression HCP Connectivity lmer = Gradient_Eigenvalues ~ Age + ICV + random nested effect(family relatedness/twin status)
 
@@ -264,6 +310,12 @@ HCP_array_aligned_mpc_G3 = read.csv(paste(resdir_hcp, 'array_aligned_mpc_G3.csv'
 # Geodesic distances (mean of top 10% of functional connections)
 HCP_mean_geodesic_distances = read.csv(paste(resdir_hcp, 'mean_geodesic_distances.csv', sep = ''), fileEncoding = 'UTF-8-BOM')
 
+# Mean geodesic distances of mean connectivity profiles by sex (ie each subject has the mean (by seed region) of the geodesic distance of the most frequent top 10% connections for their sex)
+HCP_recomp_sub_mean_geodesic_distances_most_frequent_connections_bysexprofile = read.csv(paste(resdir_hcp, 'recomp_sub_mean_geodesic_distances_most_frequent_connections_bysexprofile.csv', sep = ''), fileEncoding = 'UTF-8-BOM')
+
+# mean fc strength by seed for top 10% connections computed at the individual level
+HCP_mean_fc_strengths_top10 = read.csv(paste(resdir_hcp, 'mean_fc_strengths_top10.csv', sep = ''), fileEncoding = 'UTF-8-BOM')
+
 
 
 # class(array_aligned_G1)
@@ -313,6 +365,7 @@ HCP_lmer_fc_G3_sex_contrast_res = lmer.hcp_sex_contrast(df_dv = HCP_array_aligne
 HCP_lmer_fc_G1_rescaled_sex_contrast_res = lmer.hcp_sex_contrast(df_dv = HCP_array_aligned_fc_G1_rescaled, df_iv = HCP_demographics_cleaned_final)
 
 HCP_lmer_hemi_fc_G1_sex_contrast_res = lmer.hcp_sex_contrast(df_dv = HCP_hemi_array_aligned_fc_G1, df_iv = HCP_demographics_cleaned_final)
+
 
 # number of significant parcels
 sum(HCP_lmer_fc_G1_sex_contrast_res$q_val < 0.05, na.rm=TRUE)  # other way: length(which(G1_lm_res$q_val < 0.05))
@@ -371,6 +424,20 @@ sum(HCP_lmer_fc_G1_geo_contrast_res$q_val < 0.05, na.rm=TRUE)
 sum(HCP_lmer_hemi_fc_G1_geo_contrast_res$q_val < 0.05, na.rm=TRUE) 
 
 
+##### MPC G1 effects in model = DV ~ Sex + Age + ICV + MPC G1 + random nested effect(family relatedness/twin status)
+
+# run model
+HCP_lmer_fc_G1_mpc_G1_contrast_res = lmer.hcp_mpc_contrast(df_dv = HCP_array_aligned_fc_G1, df_iv = HCP_demographics_cleaned_final, df_mpc = HCP_array_aligned_mpc_G1)
+HCP_lmer_hemi_fc_G1_mpc_G1_contrast_res = lmer.hcp_mpc_contrast(df_dv = HCP_hemi_array_aligned_fc_G1, df_iv = HCP_demographics_cleaned_final, df_mpc = HCP_array_aligned_mpc_G1)
+
+
+# number of significant parcels
+sum(HCP_lmer_fc_G1_mpc_G1_contrast_res$q_val < 0.05, na.rm=TRUE) 
+sum(HCP_lmer_hemi_fc_G1_mpc_G1_contrast_res$q_val < 0.05, na.rm=TRUE) 
+
+
+
+
 ##### ICV effects within sexes in model = DV ~ Age + ICV + random nested effect(family relatedness/twin status)
 
 # run models
@@ -380,6 +447,27 @@ HCP_lmer_fc_G1_icv_contrast_within_sex_F_res = lmer.hcp_icv_contrast_within_sex(
 # number of significant parcels
 sum(HCP_lmer_fc_G1_icv_contrast_within_sex_M_res$q_val < 0.05, na.rm=TRUE)  
 sum(HCP_lmer_fc_G1_icv_contrast_within_sex_F_res$q_val < 0.05, na.rm=TRUE) 
+
+
+
+
+##### sex effects for mean geodesic distance by seed region of top 10% connections (appearing most frequently by sex)
+
+# run model
+HCP_lmer_mean_geo_most_frequent_connections_bysexprofile_sex_contrast_res = lmer.hcp_sex_contrast(df_dv = HCP_recomp_sub_mean_geodesic_distances_most_frequent_connections_bysexprofile, df_iv = HCP_demographics_cleaned_final)
+
+# number of significant parcels
+sum(HCP_lmer_mean_geo_most_frequent_connections_bysexprofile_sex_contrast_res$q_val < 0.05, na.rm=TRUE)  
+
+
+
+##### sex effects for mean fc strength by seed for top 10% connections computed at the individual level
+
+# run model
+HCP_lmer_mean_fc_strengths_top10_sex_contrast_res = lmer.hcp_sex_contrast(df_dv = HCP_mean_fc_strengths_top10, df_iv = HCP_demographics_cleaned_final)
+
+# number of significant parcels
+sum(HCP_lmer_mean_fc_strengths_top10_sex_contrast_res$q_val < 0.05, na.rm=TRUE)  
 
 
 
@@ -402,10 +490,13 @@ write.csv(HCP_lmer_hemi_fc_G1_sex_contrast_res, paste(resdir_hcp, 'R_lmer_hemi_f
 
 
 write.csv(HCP_lmer_fc_G1_icv_contrast_res, paste(resdir_hcp, 'R_lmer_fc_G1_icv_contrast_res.csv', sep = ''), row.names = FALSE)
-write.csv(HCP_lmer_fc_G1_geo_contrast_res, paste(resdir_hcp, 'R_lmer_fc_G1_geo_contrast_res.csv', sep = ''), row.names = FALSE)
-
 write.csv(HCP_lmer_hemi_fc_G1_icv_contrast_res, paste(resdir_hcp, 'R_lmer_hemi_fc_G1_icv_contrast_res.csv', sep = ''), row.names = FALSE)
+
+write.csv(HCP_lmer_fc_G1_geo_contrast_res, paste(resdir_hcp, 'R_lmer_fc_G1_geo_contrast_res.csv', sep = ''), row.names = FALSE)
 write.csv(HCP_lmer_hemi_fc_G1_geo_contrast_res, paste(resdir_hcp, 'R_lmer_hemi_fc_G1_geo_contrast_res.csv', sep = ''), row.names = FALSE)
+
+write.csv(HCP_lmer_fc_G1_mpc_G1_contrast_res, paste(resdir_hcp, 'R_lmer_fc_G1_mpc_G1_contrast_res.csv', sep = ''), row.names = FALSE)
+write.csv(HCP_lmer_hemi_fc_G1_mpc_G1_contrast_res, paste(resdir_hcp, 'R_lmer_hemi_fc_G1_mpc_G1_contrast_res.csv', sep = ''), row.names = FALSE)
 
 
 ### STRUCTURAL -> MPC
@@ -419,7 +510,11 @@ write.csv(HCP_lmer_geo_sex_contrast_res, paste(resdir_hcp, 'R_lmer_geo_sex_contr
 
 
 
+### sex effects for mean geodesic distance by seed region of top 10% connections (appearing most frequently by sex)
+write.csv(HCP_lmer_mean_geo_most_frequent_connections_bysexprofile_sex_contrast_res, paste(resdir_hcp, 'R_lmer_mean_geo_most_frequent_connections_bysexprofile_sex_contrast_res.csv', sep = ''), row.names = FALSE)
 
+##### sex effects for mean fc strength by seed for top 10% connections computed at the individual level
+write.csv(HCP_lmer_mean_fc_strengths_top10_sex_contrast_res, paste(resdir_hcp, 'R_lmer_mean_fc_strengths_top10_sex_contrast_res.csv', sep = ''), row.names = FALSE)
 
 
 ############################################################################################################################################################################################################
